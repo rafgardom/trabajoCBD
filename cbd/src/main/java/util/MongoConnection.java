d12219146823a880de4f8ab67d750b483a4488ad
rafgardom <rafae@DESKTOP-H74978B>
2017-04-29 13:24:18 +0200

    Arrancar servidor desde java

--- cbd/src/main/java/util/MongoConnection.java
+++ cbd/src/main/java/util/MongoConnection.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.UnknownHostException;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
@@ -14,5 +18,31 @@ public class MongoConnection {
 		return db;
 	}
 	
+	public static void runServer(String path){
+		System.out.println("Abriendo consola de comandos...");
+		
+		try{
+			File file = new File(path);
+			Process process = Runtime.getRuntime().exec(file.getAbsolutePath());
+			
+			System.out.println(file.getAbsolutePath());
+			
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(process.getInputStream()));
+			
+			String line = null;
+			while((line = br.readLine()) != null){
+				System.out.println(line);
+			}
+			
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 	
+	
+	public static void main(String[] args) {
+		String path = "C:\\Program Files\\MongoDB\\Server\\3.4\\bin\\mongod.exe";
+		runServer(path);
+	}
 }
